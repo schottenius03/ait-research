019b5ae023fe406268dbac05da45b5939bd090b7
Kailing Schottenius <kailing.schottenius@gmail.com>
2026-04-05 01:36:47 +1100

    + Search Web Form with Database Connection Manager and GridView

---
+++ aitr-connect/Search.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="aitr_connect.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnBackToDefault" runat="server" Text="< Back" BorderStyle="None" BackColor="White" BorderWidth="0" />
+            <br />
+            <br />
+            <asp:Label ID="lblTitle" runat="server" Text="Search" Font-Size="XX-Large" Font-Bold="True" ForeColor="#003399" ></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="tbxSearch" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" Text="Search" />
+            <br />
+            <br />
+            <asp:GridView ID="gvUser" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ aitr-connect/Search.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aitr_connect
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // tea - straw - connect - poke - get soda - consume
+            String myConnectionString = "Data Source=SQL8012.site4now.net;Initial Catalog=db_9ab8b7_126dda14621;User Id=db_9ab8b7_126dda14621_admin;Password=DG5py76V;";
+
+
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = myConnectionString;
+
+            myconn.Open();
+
+            SqlCommand myCmd = new SqlCommand("SELECT * FROM TabUser", myconn);
+            SqlDataReader reader = myCmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("User ID", typeof(Int32));
+            dt.Columns.Add("Username", typeof(String));
+            dt.Columns.Add("Password", typeof(String));
+            dt.Columns.Add("User Level", typeof(Int32));
+
+            while (reader.Read())
+            {
+                DataRow row = dt.NewRow();
+                row["User ID"] = reader["UID"];
+                row["Username"] = reader["UserName"];
+                row["Password"] = reader["Password"];
+                row["User Level"] = reader["UserLevel"];
+
+                dt.Rows.Add(row);
+            }
+
+            gvUser.DataSource = dt;
+            gvUser.DataBind(); // bind gv to reader
+
+            myconn.Close();
+        }
+    }
+}
