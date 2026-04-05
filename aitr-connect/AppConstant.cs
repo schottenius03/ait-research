df3f3067f52f17569821d1de5639a8c557a8f58b
Kailing Schottenius <kailing.schottenius@gmail.com>
2026-04-05 13:26:06 +1000

    + Error handling on RegisterPage

--- aitr-connect/AppConstant.cs
+++ aitr-connect/AppConstant.cs
@@ -10,7 +10,7 @@ namespace aitr_connect
         // DB connection management for multiple enviroments
         public class Connection
         {
-            public const String DevConnectionString = "Data Source=SQL8012.site4now.net;Initial Catalog=db_9ab8b7_126dda14621;User Id=db_9ab8b7_126dda14621_admin;Password=DG5py76V;";
+            public const String DevConnectionString = "Data Source=SQLx8012.site4now.net;Initial Catalog=db_9ab8b7_126dda14621;User Id=db_9ab8b7_126dda14621_admin;Password=DG5py76V;";
             public const String TestConnectionString = "";
             public const String ProdConnectionString = "";
         }
--- aitr-connect/Register.aspx.cs
+++ aitr-connect/Register.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,53 @@ namespace aitr_connect
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // get connection
+            String myConnectionString = ConfigurationManager.ConnectionStrings["KailingConnectionString"].ConnectionString;
 
+
+            if (myConnectionString.ToUpper().Equals("DEV"))
+            {
+                myConnectionString = AppConstant.Connection.DevConnectionString;
+            }
+            else if (myConnectionString.ToUpper().Equals("TEST"))
+            {
+                myConnectionString = AppConstant.Connection.TestConnectionString;
+            }
+            else if (myConnectionString.ToUpper().Equals("PROD"))
+            {
+                myConnectionString = AppConstant.Connection.ProdConnectionString;
+            }
+            else
+            {
+                myConnectionString = "";
+            }
+
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = myConnectionString;
+
+            try
+            {
+                myconn.Open();
+            }
+            catch (InvalidOperationException ex)
+            {
+                lblTitle.Text = "Interal operation error!!! Contact admin";
+            }
+
+            catch (ConfigurationErrorsException ex)
+            {
+                lblTitle.Text = "Interal configuration error!!! Contact admin";
+            }
+
+            catch (SqlException ex)
+            {
+                lblTitle.Text = "Database general error!!! Try again later.";
+            }
+
+            catch (Exception ex)
+            {
+                lblTitle.Text = "General system error!!! Try again later.";
+            }
         }
 
         protected void btnBackToDefault_Click(object sender, EventArgs e)
